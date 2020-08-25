ed2182dd543af8ac0a314db75eaca174b767bd26
qingbei <qingbei@pinduoduo.com>
2020-08-25 18:00:22 +0800

    update

--- data_model/vec.hpp
+++ data_model/vec.hpp
@@ -1,6 +1,7 @@
 /*************************************************
  * @author Bin Qu
  * @email benquickdenn@foxmail.com
+ * @suppress
 *************************************************/
 
 #ifndef VEC_HPP
@@ -9,7 +10,7 @@
 #include "../utilities/configure.hpp"
 
 // 向量类
-template <typename T> 
+template <typename T>
 struct Vec
 {
 };
@@ -20,89 +21,142 @@ class Vec<float>
 {
 private:
     mmf _mm;
+
 public:
-    Vec(){}
-    Vec(const mmf & _mm) { this->_mm = _mm; }
-    ~Vec(){}
+    Vec() {}
+    Vec(const mmf &_mm) { this->_mm = _mm; }
+    ~Vec() {}
 
     force_inline mmf mm() const { return _mm; }
-    void setmm(const mmf& _mm) { this->_mm = _mm; }
+    void setmm(const mmf &_mm) { this->_mm = _mm; }
     static size_t mmsize();
 
     void load(const float *_arr) { _mm = _vloadf(_arr); }
     void store(float *_arr) { _vstoref(_arr, _mm); }
 
-    force_inline Vec<float> & operator = (const Vec<float> & v) { this->_mm = v.mm(); return *this; }
-    force_inline Vec<float> & operator = (const mmf & _mm) { this->_mm = _mm; return *this; }
-//#if mmf!=float
-    Vec(const float & f) { _mm = _vset1f(f); }
-    force_inline Vec<float> & operator = (const float & f) { _mm = _vset1f(f); return *this; }
-//#endif
+    force_inline Vec<float> &operator=(const Vec<float> &v)
+    {
+        this->_mm = v.mm();
+        return *this;
+    }
+    force_inline Vec<float> &operator=(const mmf &_mm)
+    {
+        this->_mm = _mm;
+        return *this;
+    }
+    //#if mmf!=float
+    Vec(const float &f) { _mm = _vset1f(f); }
+    force_inline Vec<float> &operator=(const float &f)
+    {
+        _mm = _vset1f(f);
+        return *this;
+    }
+    //#endif
 };
 
 // 向量加
-static force_inline Vec<float> operator + (const Vec<float> & v1, const Vec<float> & v2)
-{ return Vec<float>(_vaddf(v1.mm(), v2.mm())); }
+static force_inline Vec<float> operator+(const Vec<float> &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vaddf(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<float> operator + (const Vec<float> & v1, const mmf & v2)
-{ return Vec<float>(_vaddf(v1.mm(), v2)); }
+static force_inline Vec<float> operator+(const Vec<float> &v1, const mmf &v2)
+{
+    return Vec<float>(_vaddf(v1.mm(), v2));
+}
 
-static force_inline Vec<float> operator + (const mmf & v1, const Vec<float> & v2)
-{ return Vec<float>(_vaddf(v1, v2.mm())); }
+static force_inline Vec<float> operator+(const mmf &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vaddf(v1, v2.mm()));
+}
 
-static force_inline Vec<float> operator + (const Vec<float> & v1, const float & f)
-{ return Vec<float>(_vaddf(v1.mm(), _vset1f(f))); }
+static force_inline Vec<float> operator+(const Vec<float> &v1, const float &f)
+{
+    return Vec<float>(_vaddf(v1.mm(), _vset1f(f)));
+}
 
-static force_inline Vec<float> operator + (const float & f, const Vec<float> & v2)
-{ return Vec<float>(_vaddf(_vset1f(f), v2.mm())); }
+static force_inline Vec<float> operator+(const float &f, const Vec<float> &v2)
+{
+    return Vec<float>(_vaddf(_vset1f(f), v2.mm()));
+}
 
 // 向量减
-static force_inline Vec<float> operator - (const Vec<float> & v1, const Vec<float> & v2)
-{ return Vec<float>(_vsubf(v1.mm(), v2.mm())); }
+static force_inline Vec<float> operator-(const Vec<float> &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vsubf(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<float> operator - (const Vec<float> & v1, const mmf & v2)
-{ return Vec<float>(_vsubf(v1.mm(), v2)); }
+static force_inline Vec<float> operator-(const Vec<float> &v1, const mmf &v2)
+{
+    return Vec<float>(_vsubf(v1.mm(), v2));
+}
 
-static force_inline Vec<float> operator - (const mmf & v1, const Vec<float> & v2)
-{ return Vec<float>(_vsubf(v1, v2.mm())); }
+static force_inline Vec<float> operator-(const mmf &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vsubf(v1, v2.mm()));
+}
 
-static force_inline Vec<float> operator - (const Vec<float> & v1, const float & f)
-{ return Vec<float>(_vsubf(v1.mm(), _vset1f(f))); }
+static force_inline Vec<float> operator-(const Vec<float> &v1, const float &f)
+{
+    return Vec<float>(_vsubf(v1.mm(), _vset1f(f)));
+}
 
-static force_inline Vec<float> operator - (const float & f, const Vec<float> & v2)
-{ return Vec<float>(_vsubf(_vset1f(f), v2.mm())); }
+static force_inline Vec<float> operator-(const float &f, const Vec<float> &v2)
+{
+    return Vec<float>(_vsubf(_vset1f(f), v2.mm()));
+}
 
 // 向量乘
-static force_inline Vec<float> operator * (const Vec<float> & v1, const Vec<float> & v2)
-{ return Vec<float>(_vmulf(v1.mm(), v2.mm())); }
+static force_inline Vec<float> operator*(const Vec<float> &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vmulf(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<float> operator * (const Vec<float> & v1, const mmf & v2)
-{ return Vec<float>(_vmulf(v1.mm(), v2)); }
+static force_inline Vec<float> operator*(const Vec<float> &v1, const mmf &v2)
+{
+    return Vec<float>(_vmulf(v1.mm(), v2));
+}
 
-static force_inline Vec<float> operator * (const mmf & v1, const Vec<float> & v2)
-{ return Vec<float>(_vmulf(v1, v2.mm())); }
+static force_inline Vec<float> operator*(const mmf &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vmulf(v1, v2.mm()));
+}
 
-static force_inline Vec<float> operator * (const Vec<float> & v1, const float & f)
-{ return Vec<float>(_vmulf(v1.mm(), _vset1f(f))); }
+static force_inline Vec<float> operator*(const Vec<float> &v1, const float &f)
+{
+    return Vec<float>(_vmulf(v1.mm(), _vset1f(f)));
+}
 
-static force_inline Vec<float> operator * (const float & f, const Vec<float> & v2)
-{ return Vec<float>(_vmulf(_vset1f(f), v2.mm())); }
+static force_inline Vec<float> operator*(const float &f, const Vec<float> &v2)
+{
+    return Vec<float>(_vmulf(_vset1f(f), v2.mm()));
+}
 
 // 向量除
-static force_inline Vec<float> operator / (const Vec<float> & v1, const Vec<float> & v2)
-{ return Vec<float>(_vdivf(v1.mm(), v2.mm())); }
+static force_inline Vec<float> operator/(const Vec<float> &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vdivf(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<float> operator / (const Vec<float> & v1, const mmf & v2)
-{ return Vec<float>(_vdivf(v1.mm(), v2)); }
+static force_inline Vec<float> operator/(const Vec<float> &v1, const mmf &v2)
+{
+    return Vec<float>(_vdivf(v1.mm(), v2));
+}
 
-static force_inline Vec<float> operator / (const mmf & v1, const Vec<float> & v2)
-{ return Vec<float>(_vdivf(v1, v2.mm())); }
+static force_inline Vec<float> operator/(const mmf &v1, const Vec<float> &v2)
+{
+    return Vec<float>(_vdivf(v1, v2.mm()));
+}
 
-static force_inline Vec<float> operator / (const Vec<float> & v1, const float & f)
-{ return Vec<float>(_vdivf(v1.mm(), _vset1f(f))); }
+static force_inline Vec<float> operator/(const Vec<float> &v1, const float &f)
+{
+    return Vec<float>(_vdivf(v1.mm(), _vset1f(f)));
+}
 
-static force_inline Vec<float> operator / (const float & f, const Vec<float> & v2)
-{ return Vec<float>(_vdivf(_vset1f(f), v2.mm())); }
+static force_inline Vec<float> operator/(const float &f, const Vec<float> &v2)
+{
+    return Vec<float>(_vdivf(_vset1f(f), v2.mm()));
+}
 
 // 双精度向量类
 template <>
@@ -110,101 +164,166 @@ class Vec<double>
 {
 private:
     mmd _mm;
+
 public:
-    Vec(){}
-    Vec(const mmd & _mm) { this->_mm = _mm; }
-    ~Vec(){}
+    Vec() {}
+    Vec(const mmd &_mm) { this->_mm = _mm; }
+    ~Vec() {}
 
     force_inline mmd mm() const { return _mm; }
-    void setmm(const mmd& _mm) { this->_mm = _mm; }
+    void setmm(const mmd &_mm) { this->_mm = _mm; }
     static size_t mmsize();
 
     void load(const double *_arr) { _mm = _vloadd(_arr); }
     void store(double *_arr) { _vstored(_arr, _mm); }
 
-    force_inline Vec<double> & operator = (const Vec<double> & v) { this->_mm = v.mm(); return *this; }
-    force_inline Vec<double> & operator = (const mmd & _mm) { this->_mm = _mm; return *this; }
-//#if mmd!=double
-    Vec(const double & d) { _mm = _vset1d(d); }
-    force_inline Vec<double> & operator = (const double & d) { _mm = _vset1d(d); return *this; }
-//#endif
+    force_inline Vec<double> &operator=(const Vec<double> &v)
+    {
+        this->_mm = v.mm();
+        return *this;
+    }
+    force_inline Vec<double> &operator=(const mmd &_mm)
+    {
+        this->_mm = _mm;
+        return *this;
+    }
+    //#if mmd!=double
+    Vec(const double &d) { _mm = _vset1d(d); }
+    force_inline Vec<double> &operator=(const double &d)
+    {
+        _mm = _vset1d(d);
+        return *this;
+    }
+    //#endif
 };
 
 // 向量加
-static force_inline Vec<double> operator + (const Vec<double> & v1, const Vec<double> & v2)
-{ return Vec<double>(_vaddd(v1.mm(), v2.mm())); }
+static force_inline Vec<double> operator+(const Vec<double> &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vaddd(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<double> operator + (const Vec<double> & v1, const mmd & v2)
-{ return Vec<double>(_vaddd(v1.mm(), v2)); }
+static force_inline Vec<double> operator+(const Vec<double> &v1, const mmd &v2)
+{
+    return Vec<double>(_vaddd(v1.mm(), v2));
+}
 
-static force_inline Vec<double> operator + (const mmd & v1, const Vec<double> & v2)
-{ return Vec<double>(_vaddd(v1, v2.mm())); }
+static force_inline Vec<double> operator+(const mmd &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vaddd(v1, v2.mm()));
+}
 
-static force_inline Vec<double> operator + (const Vec<double> & v1, const double & d)
-{ return Vec<double>(_vaddd(v1.mm(), _vset1d(d))); }
+static force_inline Vec<double> operator+(const Vec<double> &v1, const double &d)
+{
+    return Vec<double>(_vaddd(v1.mm(), _vset1d(d)));
+}
 
-static force_inline Vec<double> operator + (const double & d, const Vec<double> & v2)
-{ return Vec<double>(_vaddd(_vset1d(d), v2.mm())); }
+static force_inline Vec<double> operator+(const double &d, const Vec<double> &v2)
+{
+    return Vec<double>(_vaddd(_vset1d(d), v2.mm()));
+}
 
 // 向量减
-static force_inline Vec<double> operator - (const Vec<double> & v1, const Vec<double> & v2)
-{ return Vec<double>(_vsubd(v1.mm(), v2.mm())); }
+static force_inline Vec<double> operator-(const Vec<double> &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vsubd(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<double> operator - (const Vec<double> & v1, const mmd & v2)
-{ return Vec<double>(_vsubd(v1.mm(), v2)); }
+static force_inline Vec<double> operator-(const Vec<double> &v1, const mmd &v2)
+{
+    return Vec<double>(_vsubd(v1.mm(), v2));
+}
 
-static force_inline Vec<double> operator - (const mmd & v1, const Vec<double> & v2)
-{ return Vec<double>(_vsubd(v1, v2.mm())); }
+static force_inline Vec<double> operator-(const mmd &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vsubd(v1, v2.mm()));
+}
 
-static force_inline Vec<double> operator - (const Vec<double> & v1, const double & d)
-{ return Vec<double>(_vsubd(v1.mm(), _vset1d(d))); }
+static force_inline Vec<double> operator-(const Vec<double> &v1, const double &d)
+{
+    return Vec<double>(_vsubd(v1.mm(), _vset1d(d)));
+}
 
-static force_inline Vec<double> operator - (const double & d, const Vec<double> & v2)
-{ return Vec<double>(_vsubd(_vset1d(d), v2.mm())); }
+static force_inline Vec<double> operator-(const double &d, const Vec<double> &v2)
+{
+    return Vec<double>(_vsubd(_vset1d(d), v2.mm()));
+}
 
 // 向量乘
-static force_inline Vec<double> operator * (const Vec<double> & v1, const Vec<double> & v2)
-{ return Vec<double>(_vmuld(v1.mm(), v2.mm())); }
+static force_inline Vec<double> operator*(const Vec<double> &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vmuld(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<double> operator * (const Vec<double> & v1, const mmd & v2)
-{ return Vec<double>(_vmuld(v1.mm(), v2)); }
+static force_inline Vec<double> operator*(const Vec<double> &v1, const mmd &v2)
+{
+    return Vec<double>(_vmuld(v1.mm(), v2));
+}
 
-static force_inline Vec<double> operator * (const mmd & v1, const Vec<double> & v2)
-{ return Vec<double>(_vmuld(v1, v2.mm())); }
+static force_inline Vec<double> operator*(const mmd &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vmuld(v1, v2.mm()));
+}
 
-static force_inline Vec<double> operator * (const Vec<double> & v1, const double & d)
-{ return Vec<double>(_vmuld(v1.mm(), _vset1d(d))); }
+static force_inline Vec<double> operator*(const Vec<double> &v1, const double &d)
+{
+    return Vec<double>(_vmuld(v1.mm(), _vset1d(d)));
+}
 
-static force_inline Vec<double> operator * (const double & d, const Vec<double> & v2)
-{ return Vec<double>(_vmuld(_vset1d(d), v2.mm())); }
+static force_inline Vec<double> operator*(const double &d, const Vec<double> &v2)
+{
+    return Vec<double>(_vmuld(_vset1d(d), v2.mm()));
+}
 
 // 向量除
-static force_inline Vec<double> operator / (const Vec<double> & v1, const Vec<double> & v2)
-{ return Vec<double>(_vdivd(v1.mm(), v2.mm())); }
+static force_inline Vec<double> operator/(const Vec<double> &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vdivd(v1.mm(), v2.mm()));
+}
 
-static force_inline Vec<double> operator / (const Vec<double> & v1, const mmd & v2)
-{ return Vec<double>(_vdivd(v1.mm(), v2)); }
+static force_inline Vec<double> operator/(const Vec<double> &v1, const mmd &v2)
+{
+    return Vec<double>(_vdivd(v1.mm(), v2));
+}
 
-static force_inline Vec<double> operator / (const mmd & v1, const Vec<double> & v2)
-{ return Vec<double>(_vdivd(v1, v2.mm())); }
+static force_inline Vec<double> operator/(const mmd &v1, const Vec<double> &v2)
+{
+    return Vec<double>(_vdivd(v1, v2.mm()));
+}
 
-static force_inline Vec<double> operator / (const Vec<double> & v1, const double & d)
-{ return Vec<double>(_vdivd(v1.mm(), _vset1d(d))); }
+static force_inline Vec<double> operator/(const Vec<double> &v1, const double &d)
+{
+    return Vec<double>(_vdivd(v1.mm(), _vset1d(d)));
+}
 
-static force_inline Vec<double> operator / (const double & d, const Vec<double> & v2)
-{ return Vec<double>(_vdivd(_vset1d(d), v2.mm())); }
+static force_inline Vec<double> operator/(const double &d, const Vec<double> &v2)
+{
+    return Vec<double>(_vdivd(_vset1d(d), v2.mm()));
+}
 
 #if defined(__AVX512F__)
-size_t Vec<float>::mmsize() { return 16; }
+size_t Vec<float>::mmsize()
+{
+    return 16;
+}
 size_t Vec<double>::mmsize() { return 8; }
 #elif defined(__AVX__)
-size_t Vec<float>::mmsize() { return 8; }
+size_t Vec<float>::mmsize()
+{
+    return 8;
+}
 size_t Vec<double>::mmsize() { return 4; }
 #elif defined(__SSE__)
-size_t Vec<float>::mmsize() { return 4; }
+size_t Vec<float>::mmsize()
+{
+    return 4;
+}
 size_t Vec<double>::mmsize() { return 2; }
 #else
-size_t Vec<float>::mmsize() { return 1; }
+size_t Vec<float>::mmsize()
+{
+    return 1;
+}
 size_t Vec<double>::mmsize() { return 1; }
 #endif
 
--- utilities/configure.hpp
+++ utilities/configure.hpp
@@ -26,81 +26,97 @@
 
 // single and double type definition
 #if defined(__AVX512F__)
-    typedef __m512 mmf;
-    typedef __m512d mmd;
-    #define STRIDEF 16
-    #define STRIDED 8
-    #define _vloadf(arr) _mm512_loadu_ps(arr)
-    #define _vloadd(arr) _mm512_loadu_pd(arr)
-    #define _vstoref(arr, mm)  _mm512_storeu_ps(arr, mm)
-    #define _vstored(arr, mm)  _mm512_storeu_pd(arr, mm)
-    #define _vset1f(f) _mm512_set1_ps(f)
-    #define _vset1d(d) _mm512_set1_pd(d)
-    #define _vaddf(mm1, mm2) _mm512_add_ps(mm1, mm2)
-    #define _vaddd(mm1, mm2) _mm512_add_pd(mm1, mm2)
-    #define _vsubf(mm1, mm2) _mm512_sub_ps(mm1, mm2)
-    #define _vsubd(mm1, mm2) _mm512_sub_pd(mm1, mm2)
-    #define _vmulf(mm1, mm2) _mm512_mul_ps(mm1, mm2)
-    #define _vmuld(mm1, mm2) _mm512_mul_pd(mm1, mm2)
-    #define _vdivf(mm1, mm2) _mm512_div_ps(mm1, mm2)
-    #define _vdivd(mm1, mm2) _mm512_div_pd(mm1, mm2)
+typedef __m512 mmf;
+typedef __m512d mmd;
+#define STRIDEF 16
+#define STRIDED 8
+#define _vloadf(arr) _mm512_loadu_ps(arr)
+#define _vloadd(arr) _mm512_loadu_pd(arr)
+#define _vstoref(arr, mm) _mm512_storeu_ps(arr, mm)
+#define _vstored(arr, mm) _mm512_storeu_pd(arr, mm)
+#define _vset1f(f) _mm512_set1_ps(f)
+#define _vset1d(d) _mm512_set1_pd(d)
+#define _vsetrf(f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15) _mm512_setr_ps(f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15)
+#define _vsetrd(d0, d1, d2, d3, d4, d5, d6, d7) _mm512_setr_pd(d0, d1, d2, d3, d4, d5, d6, d7)
+#define _vaddf(mm1, mm2) _mm512_add_ps(mm1, mm2)
+#define _vaddd(mm1, mm2) _mm512_add_pd(mm1, mm2)
+#define _vsubf(mm1, mm2) _mm512_sub_ps(mm1, mm2)
+#define _vsubd(mm1, mm2) _mm512_sub_pd(mm1, mm2)
+#define _vmulf(mm1, mm2) _mm512_mul_ps(mm1, mm2)
+#define _vmuld(mm1, mm2) _mm512_mul_pd(mm1, mm2)
+#define _vdivf(mm1, mm2) _mm512_div_ps(mm1, mm2)
+#define _vdivd(mm1, mm2) _mm512_div_pd(mm1, mm2)
+#define _vreduce_addd(mm) _mm512_reduce_add_pd(mm)
+#define _vreduce_addf(mm) _mm512_reduce_add_ps(mm)
 #elif defined(__AVX__)
-    typedef __m256 mmf;
-    typedef __m256d mmd;
-    #define STRIDEF 8
-    #define STRIDED 4
-    #define _vloadf(arr) _mm256_loadu_ps(arr)
-    #define _vloadd(arr) _mm256_loadu_pd(arr)
-    #define _vstoref(arr, mm)  _mm256_storeu_ps(arr, mm)
-    #define _vstored(arr, mm)  _mm256_storeu_pd(arr, mm)
-    #define _vset1f(f) _mm256_set1_ps(f)
-    #define _vset1d(d) _mm256_set1_pd(d)
-    #define _vaddf(mm1, mm2) _mm256_add_ps(mm1, mm2)
-    #define _vaddd(mm1, mm2) _mm256_add_pd(mm1, mm2)
-    #define _vsubf(mm1, mm2) _mm256_sub_ps(mm1, mm2)
-    #define _vsubd(mm1, mm2) _mm256_sub_pd(mm1, mm2)
-    #define _vmulf(mm1, mm2) _mm256_mul_ps(mm1, mm2)
-    #define _vmuld(mm1, mm2) _mm256_mul_pd(mm1, mm2)
-    #define _vdivf(mm1, mm2) _mm256_div_ps(mm1, mm2)
-    #define _vdivd(mm1, mm2) _mm256_div_pd(mm1, mm2)
+typedef __m256 mmf;
+typedef __m256d mmd;
+#define STRIDEF 8
+#define STRIDED 4
+#define _vloadf(arr) _mm256_loadu_ps(arr)
+#define _vloadd(arr) _mm256_loadu_pd(arr)
+#define _vstoref(arr, mm) _mm256_storeu_ps(arr, mm)
+#define _vstored(arr, mm) _mm256_storeu_pd(arr, mm)
+#define _vset1f(f) _mm256_set1_ps(f)
+#define _vset1d(d) _mm256_set1_pd(d)
+#define _vsetrf(f0, f1, f2, f3, f4, f5, f6, f7) _mm256_setr_ps(f0, f1, f2, f3, f4, f5, f6, f7)
+#define _vsetrd(d0, d1, d2, d3) _mm256_setr_pd(d0, d1, d2, d3)
+#define _vaddf(mm1, mm2) _mm256_add_ps(mm1, mm2)
+#define _vaddd(mm1, mm2) _mm256_add_pd(mm1, mm2)
+#define _vsubf(mm1, mm2) _mm256_sub_ps(mm1, mm2)
+#define _vsubd(mm1, mm2) _mm256_sub_pd(mm1, mm2)
+#define _vmulf(mm1, mm2) _mm256_mul_ps(mm1, mm2)
+#define _vmuld(mm1, mm2) _mm256_mul_pd(mm1, mm2)
+#define _vdivf(mm1, mm2) _mm256_div_ps(mm1, mm2)
+#define _vdivd(mm1, mm2) _mm256_div_pd(mm1, mm2)
+#define _vreduce_addd(mm) _mm256_reduce_add_pd(mm)
+#define _vreduce_addf(mm) _mm256_reduce_add_ps(mm)
 #elif defined(__SSE__)
-    typedef __m128 mmf;
-    typedef __m128d mmd;
-    #define STRIDEF 4
-    #define STRIDED 2
-    #define _vloadf(arr) _mm_loadu_ps(arr)
-    #define _vloadd(arr) _mm_loadu_pd(arr)
-    #define _vstoref(arr, mm)  _mm_storeu_ps(arr, mm)
-    #define _vstored(arr, mm)  _mm_storeu_pd(arr, mm)
-    #define _vset1f(f) _mm_set1_ps(f)
-    #define _vset1d(d) _mm_set1_pd(d)
-    #define _vaddf(mm1, mm2) _mm_add_ps(mm1, mm2)
-    #define _vaddd(mm1, mm2) _mm_add_pd(mm1, mm2)
-    #define _vsubf(mm1, mm2) _mm_sub_ps(mm1, mm2)
-    #define _vsubd(mm1, mm2) _mm_sub_pd(mm1, mm2)
-    #define _vmulf(mm1, mm2) _mm_mul_ps(mm1, mm2)
-    #define _vmuld(mm1, mm2) _mm_mul_pd(mm1, mm2)
-    #define _vdivf(mm1, mm2) _mm_div_ps(mm1, mm2)
-    #define _vdivd(mm1, mm2) _mm_div_pd(mm1, mm2)
+typedef __m128 mmf;
+typedef __m128d mmd;
+#define STRIDEF 4
+#define STRIDED 2
+#define _vloadf(arr) _mm_loadu_ps(arr)
+#define _vloadd(arr) _mm_loadu_pd(arr)
+#define _vstoref(arr, mm) _mm_storeu_ps(arr, mm)
+#define _vstored(arr, mm) _mm_storeu_pd(arr, mm)
+#define _vset1f(f) _mm_set1_ps(f)
+#define _vset1d(d) _mm_set1_pd(d)
+#define _vsetrf(f0, f1, f2, f3) _mm_setr_ps(f0, f1, f2, f3)
+#define _vsetrd(d0, d1) _mm_setr_pd(d0, d1)
+#define _vaddf(mm1, mm2) _mm_add_ps(mm1, mm2)
+#define _vaddd(mm1, mm2) _mm_add_pd(mm1, mm2)
+#define _vsubf(mm1, mm2) _mm_sub_ps(mm1, mm2)
+#define _vsubd(mm1, mm2) _mm_sub_pd(mm1, mm2)
+#define _vmulf(mm1, mm2) _mm_mul_ps(mm1, mm2)
+#define _vmuld(mm1, mm2) _mm_mul_pd(mm1, mm2)
+#define _vdivf(mm1, mm2) _mm_div_ps(mm1, mm2)
+#define _vdivd(mm1, mm2) _mm_div_pd(mm1, mm2)
+#define _vreduce_addd(mm) _mm_reduce_add_pd(mm)
+#define _vreduce_addf(mm) _mm_reduce_add_ps(mm)
 #else
-    typedef float mmf;
-    typedef double mmd;
-    #define STRIDEF 1
-    #define STRIDED 1
-    #define _vloadf(arr) *(arr)
-    #define _vloadd(arr) *(arr)
-    #define _vstoref(arr, mm)  *(arr)=mm
-    #define _vstored(arr, mm)  *(arr)=mm
-    #define _vset1f(f) f
-    #define _vset1d(d) d
-    #define _vaddf(mm1, mm2) mm1+mm2
-    #define _vaddd(mm1, mm2) mm1+mm2
-    #define _vsubf(mm1, mm2) mm1-mm2
-    #define _vsubd(mm1, mm2) mm1-mm2
-    #define _vmulf(mm1, mm2) mm1*mm2
-    #define _vmuld(mm1, mm2) mm1*mm2
-    #define _vdivf(mm1, mm2) mm1/mm2
-    #define _vdivd(mm1, mm2) mm1/mm2
+typedef float mmf;
+typedef double mmd;
+#define STRIDEF 1
+#define STRIDED 1
+#define _vloadf(arr) *(arr)
+#define _vloadd(arr) *(arr)
+#define _vstoref(arr, mm) *(arr) = mm
+#define _vstored(arr, mm) *(arr) = mm
+#define _vset1f(f) f
+#define _vset1d(d) d
+#define _vsetrf(f) f
+#define _vsetrd(d) d
+#define _vaddf(mm1, mm2) mm1 + mm2
+#define _vaddd(mm1, mm2) mm1 + mm2
+#define _vsubf(mm1, mm2) mm1 - mm2
+#define _vsubd(mm1, mm2) mm1 - mm2
+#define _vmulf(mm1, mm2) mm1 *mm2
+#define _vmuld(mm1, mm2) mm1 *mm2
+#define _vdivf(mm1, mm2) mm1 / mm2
+#define _vdivd(mm1, mm2) mm1 / mm2
+#define _vreduce_addd(mm) mm
+#define _vreduce_addf(mm) mm
 #endif
 
 #endif
